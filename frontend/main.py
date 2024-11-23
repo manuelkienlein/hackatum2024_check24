f8cd4db3629b2e7ebedf16d2938a2acb5b2ab953
Markus Schorer <schorer.markus@outlook.de>
2024-11-23 17:34:00 +0100

    updated front end

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import json
+import time
 import collections
+import datetime
+import requests
+
+ENDPOINT_URL = "http://localhost:80/api/offers"
 
 st.set_page_config(layout="wide")
 
@@ -14,6 +19,19 @@ if "region_num_dict" not in st.session_state:
         print("Read regions")
         d = json.load(file)
     st.session_state["region_num_dict"] = d
+
+if "current_page_index" not in st.session_state:
+    st.session_state["current_page_index"] = 0
+
+if "page_size" not in st.session_state:
+    st.session_state["page_size"] = 20    
+
+min_seat_num = 1
+only_vollkasko = 0
+min_price = 0
+max_price = 0
+car_type = None
+min_free_km = 0
 
 st.header("Go Api Go - The Best Rental Car Service", divider="red")
 
@@ -30,9 +48,7 @@ if st.session_state["current_state"] == 0:
 c1, c2,c3,c4, c5, c6, c7, c8 = st.columns([1,0.6,0.5,0.6,0.5,0.4,0.8,0.5])
 with c1:
     region_options = st.session_state["region_num_dict"].keys()
-    print(region_options)
     region_options = sorted(region_options)
-    print(region_options)
     selected = st.selectbox("Region", 
                 region_options,
                 index=None,
@@ -42,20 +58,26 @@ with c1:
     else:
         region_id = st.session_state["region_num_dict"][selected]
 with c2:
-    st.date_input("Time Range Begin", value="today", label_visibility='visible')
+    begin_date = st.date_input("Time Range Begin", value="today", label_visibility='visible')
 with c3:
-    st.time_input("Start Time Input",value="now", label_visibility="hidden")
+    begin_time = st.time_input("Start Time Input",value="now", label_visibility="hidden")
 
 with c4:
-    st.date_input("Time Range End", value="today", label_visibility="visible")
+    end_date = st.date_input("Time Range End", value="today", label_visibility="visible")
 with c5:
-    st.time_input("End Time Input",value="now", label_visibility="hidden")
+    end_time = st.time_input("End Time Input",value="now", label_visibility="hidden")
 
 with c6:
-    st.number_input(label="Days",value=10, min_value=1, step=1, placeholder="Choose the amount of days (24h)")
+    amount_days = st.number_input(label="Days",value=10, min_value=1, step=1, placeholder="Choose the amount of days (24h)")
 
 with c7:
-    st.selectbox(label="Order", options=["Price Ascending", "Price Descending"], index=0)
+    order = st.selectbox(label="Order", options=["Price Ascending", "Price Descending"], index=0)
+    if order == "Price Ascending":
+        order = "price-asc"
+    elif order == "Price Descending":
+        order = "price-desc"
+    else:
+        order = None
 with c8:
     st.write("")
     st.write("")
@@ -68,26 +90,92 @@ if st.session_state["current_state"] == 1:
         st.write(" ")
 
         st.write("**Filter Options**")
-        st.number_input("Min. Seat Number", min_value=1,step=1, max_value=10, value=1, placeholder="Min. Free Seats")
-        st.toggle("Only Vollkasko", value=False)
+        min_seat_num = st.number_input("Min. Seat Number", min_value=1,step=1, max_value=50, value=1, placeholder="Min. Free Seats")
+        only_vollkasko = st.toggle("Only Vollkasko", value=False)
 
-        st.slider(label="Price Range", min_value=0,
-                    max_value=500,
-                    value=(100, 400))
+        min_price, max_price = st.slider(label="Price Range", min_value=0,
+                    max_value=1000,
+                    value=(0, 1000))
         
-        st.multiselect("Car Type", options=["small", "sports", "luxury", "family"], placeholder="Choose type of car")
+        car_type = st.selectbox("Car Type", options=["small", "sports", "luxury", "family"],index=None, placeholder="Choose type of car")
 
-        st.number_input("Min. free Kilometers", min_value=0, step=50, value=0)
+        min_free_km = st.number_input("Min. free Kilometers", min_value=0, step=50, value=0)
     
-    # Choose Page part
+# Choose Page 
     st.write(" ")
     _, c2, c3 = st.columns([2,1,4])
     with c2:
-        st.selectbox("Num. Results per Page", options=[20,50,100], index=0)
+        page_size = st.selectbox("Num. Results per Page", options=[20,50,100], index=0)
+        if not page_size == st.session_state["page_size"]:
+            st.session_state["page_size"] = page_size
     with c3:
-        st.segmented_control("Pages",["Next Page"])
-    
+        current_page = st.session_state["current_page_index"] + 1
+        st.pills("Pages",[f"Current Page ({current_page})","Next Page"])
 
-if search_button and st.session_state["current_state"] == 0:
-    st.session_state["current_state"] = 1
-    st.rerun()
+
+## BACKEND ##################
+
+def perform_search():
+    page_index = st.session_state["current_page_index"]
+    page_size = st.session_state["page_size"]
+    print(f"""Perform search for Region {region_id}, 
+          Begin Date {begin_date}, 
+          Begin Time {begin_time}, 
+          End Date {end_date}, 
+          End Time {end_time}, 
+          Amount Days {amount_days}, 
+          Order {order},
+          Page {page_index}, 
+          PageSize {page_size},
+          MinSeatNum {min_seat_num},
+          OnlyVollkasko {only_vollkasko},
+          Price Range ({min_price},{max_price}),
+          Car Types {car_type},
+          MinFreeKM {min_free_km}""")
+    
+    combined_start_time = datetime.datetime.combine(begin_date, begin_time)
+    combined_end_time = datetime.datetime.combine(end_date, end_time)
+    unixtime_start = time.mktime(combined_start_time.timetuple())
+    unixtime_end = time.mktime(combined_end_time.timetuple())
+
+    
+    send_data = {"regionID":region_id,
+                "timeRangeStart":unixtime_start,
+                "timeRangeEnd":unixtime_end,
+                "numberDays": amount_days,
+                "sortOrder":order, 
+                "page":page_index,
+                "pageSize":page_size,
+                "priceRangeWidth":100,      # NOT IMPLEMENTED
+                "minFreeKilometerWidth":100 # NOT IMPLEMENTED
+                }
+    if min_seat_num > 1:
+        send_data.update({"minNumberSeats":min_seat_num})
+    if min_price > 0:
+        send_data.update({"minPrice":min_price})
+    if max_price > 0:
+        send_data.update({"maxPrice":max_price})
+    if not car_type is None:
+        send_data.update({"carType":car_type}) 
+    if only_vollkasko:
+        send_data.update({"onlyVollkasko":only_vollkasko})
+    if min_free_km > 0:
+        send_data.update({"minFreeKilometer":min_free_km})
+
+    print("Send Data", send_data)
+
+    # response = requests.post(ENDPOINT_URL, data={})
+    
+if search_button:
+
+    if st.session_state["current_state"] == 0:
+        st.session_state["current_state"] = 1
+        perform_search()
+        st.rerun()
+    else:
+        perform_search()
+
+
+
+
+
