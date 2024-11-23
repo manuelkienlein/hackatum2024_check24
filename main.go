a4181c386325b3f0e16eb6366efa20b47bda74a7
Manuel Kienlein <manuel.kienlein@gmx.de>
2024-11-23 09:54:23 +0100

    added go module files and more endpoint drafts

--- main.go
+++ main.go
@@ -9,5 +9,18 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
+	app.Get("/api/offers", func(c *fiber.Ctx) error {
+		return c.SendString("GET Offers!")
+	})
+
+	app.Post("/api/offers", func(c *fiber.Ctx) error {
+		// endpoint is expected to be blocking. Your solution should only return a response once the offers have been successfully processed and can be read from with new GET requests. During this blocking time, there are no requests to the GET endpoint we don't send any request that would require the offer to be present or change the expected result.
+		return c.SendString("POST Offers!")
+	})
+
+	app.Delete("/api/offers", func(c *fiber.Ctx) error {
+		return c.SendString("DELETE Offers!")
+	})
+
 	app.Listen(":3000")
 }
